f6393f6089a278eb534fc0e694e31910c831d56b
irthompson01 <36970433+irthompson01@users.noreply.github.com>
2023-11-17 16:43:59 -0500

    Update pdf_bot.py

--- pdf_bot.py
+++ pdf_bot.py
@@ -47,9 +47,48 @@ class StreamHandler(BaseCallbackHandler):
 
 llm = load_llm(llm_name, logger=logger, config={"ollama_base_url": ollama_base_url})
 
+# Streamlit UI
+styl = f"""
+<style>
+    .stChatFloatingInputContainer {{
+        bottom: 20px;
+    }}
+</style>
+"""
+st.markdown(styl, unsafe_allow_html=True)
+# create separate functions for the chat history and the chat input
+def display_chat_history():
+    if "user_input" not in st.session_state:
+        st.session_state["user_input"] = []
+    if "generated_output" not in st.session_state:
+        st.session_state["generated_output"] = []
+    if st.session_state[f"generated_output"]:
+        size = len(st.session_state[f"generated_output"])
+        # Display only the last three exchanges
+        for i in range(max(size - 3, 0), size):
+            with st.chat_message("user"):
+                st.write(st.session_state[f"user_input"][i])
+            with st.chat_message("assistant"):
+                st.write(st.session_state[f"generated_output"][i])
+        with st.container():
+            st.write("&nbsp;")
+def chat_input(qa):
+    query = st.chat_input("Ask questions about related your upload pdf files")
+    if query:
+        with st.chat_message("user"):
+            st.write(query)
+        with st.chat_message("assistant"):
+            stream_handler = StreamHandler(st.empty())
+            qa.run(query, callbacks=[stream_handler])
+            # add the query to user input
+            st.session_state[f"user_input"].append(query)
+            # add the answer to generated output
+            text = stream_handler.text
+            st.session_state[f"generated_output"].append(text)
+
 
 def main():
-    st.header("📄Chat with your pdf file")
+    st.header("📄Chat with your pdf files with history")
 
     # upload a your pdf file
     pdf = st.file_uploader("Upload your PDF", type="pdf")
@@ -83,12 +122,15 @@ def main():
             llm=llm, chain_type="stuff", retriever=vectorstore.as_retriever()
         )
 
-        # Accept user questions/query
-        query = st.text_input("Ask questions about your PDF file")
+        # # Accept user questions/query
+        # query = st.text_input("Ask questions about your PDF file")
 
-        if query:
-            stream_handler = StreamHandler(st.empty())
-            qa.run(query, callbacks=[stream_handler])
+        # if query:
+        #     stream_handler = StreamHandler(st.empty())
+        #     qa.run(query, callbacks=[stream_handler])
+
+        display_chat_history()
+        chat_input(qa)
 
 
 if __name__ == "__main__":
